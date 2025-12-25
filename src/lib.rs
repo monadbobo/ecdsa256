e0e9aed79c56d7fbfdee64e5df8ac59ff57d86d5
Simon Liu <simohayha.bobo@gmail.com>
2025-12-25 12:26:40 +0800

    Remove unused addition function and associated tests from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,3 @@ pub mod curve;
 pub mod ecdsa;
 pub mod field;
 pub mod scalar;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
